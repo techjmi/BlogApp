1fdab597cea8d2d19b84877f313ff4a307a5f8d1
MD SHAMIM AKHTER <contactshamim62@gmail.com>
2024-01-08 18:52:05 +0530

    Blog Website

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config() 
+const express= require("express")
+const mongoDB = require("./database/db")
+const Router= require("./router/router")
+const app= express()
+const PORT=8000
+const cors= require("cors")
+const corsOption={
+    origin:"http://localhost:3000",
+    methods:"GET, POST, PUT ,PATCH, DELETE, HEAD",
+    crendetials:true 
+}  
+app.use(cors(corsOption)) 
+app.use(express.json())  
+app.use("/api", Router)  
+mongoDB().then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`our server is running at port no ${PORT}`)
+    })
+}) 
